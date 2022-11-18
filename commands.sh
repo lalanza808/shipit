ae0ebbab27848212148e1719b1f87015de46c707
lza_menace <lza_menace@protonmail.com>
2022-11-18 11:58:40 -0800

    updating commands

--- commands.sh
+++ commands.sh
@@ -14,8 +14,11 @@ forge create --rpc-url http://localhost:8545 --private-key $PRIVATE_KEY src/Send
 echo -e "\n[+] Sending test Ether"
 cast send --rpc-url http://localhost:8545 --private-key $PRIVATE_KEY 0x653D2d1D10c79017b2eA5F5a6F02D9Ab6e725395 --value 0.5ether
 
-echo -e "[+] Minting 50 to deployer"
-cast send --rpc-url http://localhost:8545 --private-key $PRIVATE_KEY 0x5FbDB2315678afecb367f032d93F642f64180aa3 "mint(uint256)" 100 | grep -v logs
+echo -e "[+] Minting 500 ERC-721 tokens to deployer"
+cast send --rpc-url http://localhost:8545 --private-key $PRIVATE_KEY 0x5FbDB2315678afecb367f032d93F642f64180aa3 "mint(uint256)" 500 | grep -v logs
+
+echo -e "[+] Minting 500 ERC-1155 tokens to deployer"
+cast send --rpc-url http://localhost:8545 --private-key $PRIVATE_KEY 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512 "mint(uint256,uint256)" 1 500 | grep -v logs
 
 echo -e "[+] Setting approval for contract"
 cast send --rpc-url http://localhost:8545 --private-key $PRIVATE_KEY 0x5FbDB2315678afecb367f032d93F642f64180aa3 "setApprovalForAll(address,bool)"  0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0 true | grep -v logs
